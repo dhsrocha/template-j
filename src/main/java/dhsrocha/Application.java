d626a432673b2ddd8abbd6d3601731e524f23e31
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2020-12-16 19:54:05 +0000

    Added lombok and logging support.

--- src/main/java/dhsrocha/Application.java
+++ src/main/java/dhsrocha/Application.java
@@ -1,8 +1,13 @@
 package dhsrocha;
 
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
 public class Application {
 
-  public static void main(final String[] args) {
-    System.out.println("Application running.");
+  public static void main(final String... args) {
+    log.info("Application running.");
   }
 }
