9cda874ac52d00e9bcb2a74f509a2777ddd5dce7
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2021-01-07 07:43:01 +0000

    Moved creation logic to enums in middleware's main method.

--- mods/middleware/src/main/java/template/Main.java
+++ mods/middleware/src/main/java/template/Main.java
@@ -1,5 +1,6 @@
 package template;
 
+import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Bind;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.api.model.HostConfig;
@@ -12,9 +13,9 @@ import java.net.URI;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -26,7 +27,6 @@ import lombok.val;
 @UtilityClass
 public class Main {
 
-  private static final String DOCKER_MANAGER = "docker-manager";
   private static final String SOCKET = "/var/run/docker.sock";
 
   /**
@@ -48,33 +48,43 @@ public class Main {
     Stream.of(Props.values()).forEach(p -> log
         .info("Property {}: [{}]", p, p.get(String::valueOf)));
     // Prune
+    val names = Stream.of(Service.values()).map(s -> s.contName)
+                      .collect(Collectors.toList());
     val containers = client.listContainersCmd()
                            .withShowAll(Boolean.TRUE)
-                           .withNameFilter(List.of(DOCKER_MANAGER))
+                           .withNameFilter(names)
                            .exec();
     containers.stream()
               .map(Container::getNames).map(Arrays::toString)
               .forEach(c -> log.info("Listed container: {}", c));
     if (Props.DEV_MODE.get(Boolean::valueOf) && containers.isEmpty()) {
-      // docker-manager
-      val hostCfg = HostConfig.newHostConfig()
-                              .withRestartPolicy(RestartPolicy.alwaysRestart())
-                              .withPortBindings(
-                                  PortBinding.parse("9000:9000:9000"),
-                                  PortBinding.parse("8000:8000:8000"))
-                              // Standalone mode volume (aka portainer_data) is
-                              // created on the fly.
-                              .withBinds(Bind.parse(SOCKET + ":" + SOCKET));
-      val id = client.createContainerCmd("portainer/portainer-ce")
-                     .withDomainName("dev")
-                     .withName(DOCKER_MANAGER)
-                     .withHostConfig(hostCfg)
-                     .exec().getId();
-      client.startContainerCmd(id).exec();
-      val i = client.inspectContainerCmd(id).exec();
-      log.info("Service [{}]: [{}]", i.getName(), i.getState().getStatus());
+      Service.DOCKER_MANAGER.create(client, HostConfig
+          .newHostConfig()
+          .withRestartPolicy(RestartPolicy.alwaysRestart())
+          .withPortBindings(
+              PortBinding.parse("9000:9000:9000"),
+              PortBinding.parse("8000:8000:8000"))
+          // Standalone mode volume (aka portainer_data) is created on the fly.
+          .withBinds(Bind.parse(SOCKET + ":" + SOCKET)));
     }
     log.info("Docker auth status: [{}].", client.authCmd().exec().getStatus());
+  }
+
+  @AllArgsConstructor
+  enum Service {
+    DOCKER_MANAGER("portainer/portainer-ce", "docker-manager"),
+    ;
+    private final String imgName;
+    private final String contName;
+
+    void create(final DockerClient cli, final HostConfig cfg) {
+      try (val created = cli.createContainerCmd(imgName)) {
+        val id = created.withName(contName).withHostConfig(cfg).exec().getId();
+        cli.startContainerCmd(id).exec();
+        val i = cli.inspectContainerCmd(id).exec();
+        log.info("Service [{}]: [{}]", i.getName(), i.getState().getStatus());
+      }
+    }
   }
 
   @AllArgsConstructor
