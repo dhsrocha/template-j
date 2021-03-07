160706739900f93be80c4512b28c47217e24639a
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2021-03-07 20:57:03 +0000

    Tweaked app module's module description for opening reflection only for testable files.

--- mods/app/src/main/java/module-info.java
+++ mods/app/src/main/java/module-info.java
@@ -1,7 +1,11 @@
-open module template.app {
+module template.app {
   // System
   requires lombok;
   requires org.slf4j;
   // Application
   requires io.javalin;
+
+  // Open for testing
+  opens template;
+  opens template.feature.user;
 }
