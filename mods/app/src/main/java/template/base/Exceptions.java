69084d16ba442f25e9dc922f930ae8df6c8de02a
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2021-05-14 11:14:45 +0100

    Improved checked exception handling in Exceptions.
    * Restricted internal value for storing only exceptions which are
      mapped by Javalin;
    * Hugely gave deeper detailing to javadocs.
    * Create another method to wrap potentially wrapping function that
      does not have return.
    * Created functional interface to contain and suppress a checekd
      exception.

--- mods/app/src/main/java/template/base/Exceptions.java
+++ mods/app/src/main/java/template/base/Exceptions.java
@@ -1,7 +1,9 @@
 package template.base;
 
 import io.javalin.http.BadRequestResponse;
+import io.javalin.http.HttpResponseException;
 import io.javalin.http.NotFoundResponse;
+import java.util.concurrent.Callable;
 import java.util.function.BooleanSupplier;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -28,17 +30,17 @@ public enum Exceptions implements Supplier<RuntimeException> {
   RESOURCE_NOT_FOUND(NotFoundResponse::new),
   ;
 
-  private final Function<String, RuntimeException> ex;
+  private final Function<String, HttpResponseException> ex;
 
   /**
    * Throws a provided exception if at least one of the provided boolean
-   * conditions returns true. It is advisable to provide the computing
+   * conditions returns {@code true}. It is advisable to provide the computing
    * starting from the least computing cost.
    *
    * @param ex         An {@link RuntimeException} supplied by a string message.
    * @param conditions Indicates an undesirable conditions to trigger the
    *                   supplied exception.
-   * @throws RuntimeException The supplied exception instance.
+   * @throws HttpResponseException The supplied exception instance.
    */
   public static void throwIf(final @NonNull Supplier<RuntimeException> ex,
                              final @NonNull BooleanSupplier... conditions) {
@@ -51,12 +53,12 @@ public enum Exceptions implements Supplier<RuntimeException> {
 
   /**
    * Throws a provided exception if at least one of the provided boolean
-   * conditions returns true. It is advisable to provide the computing
+   * conditions returns {@code true}. It is advisable to provide the computing
    * starting from the least computing cost.
    *
    * @param conditions Indicates an undesirable conditions to trigger the
    *                   supplied exception.
-   * @throws RuntimeException The exception indexed by the provided item.
+   * @throws HttpResponseException The exception indexed by the provided item.
    * @see #throwIf(String, BooleanSupplier...)
    */
   public final void throwIf(final @NonNull BooleanSupplier... conditions) {
@@ -65,14 +67,14 @@ public enum Exceptions implements Supplier<RuntimeException> {
 
   /**
    * Throws a provided exception if at least one of the provided boolean
-   * conditions returns true. It is advisable to provide the computing
+   * conditions returns {@code true}. It is advisable to provide the computing
    * starting from the least computing cost.
    *
    * @param message    A custom message to throw along with the indexed
    *                   {@link #ex exception}.
    * @param conditions Indicates an undesirable conditions to trigger the
    *                   supplied exception.
-   * @throws RuntimeException The exception indexed by the provided item.
+   * @throws HttpResponseException The indexed one by the provided item.
    * @see #throwIf(Supplier, BooleanSupplier...)
    */
   public final void throwIf(final @NonNull String message,
@@ -81,46 +83,81 @@ public enum Exceptions implements Supplier<RuntimeException> {
   }
 
   /**
-   * Traps any potential {@link RuntimeException} thrown out of the provided
-   * scoped code and redirect (re-throw) it as the indexed one.
+   * Traps any potential {@link Exception checked exception} thrown out of the
+   * provided scoped code and redirect (re-throw) it as the indexed one. It is
+   * also a convenient way to write a try-catch block into an one-liner.
    *
-   * @param <R>   Type of upcoming result from the provided parameter.
-   * @param scope Computation where a {@link RuntimeException} might be thrown
-   *              from.
+   * @param <R>  Type of upcoming result from the provided parameter.
+   * @param wrap Potentially throwing wrapped computation.
    * @return Any result supplied in the provided parameter.
+   * @throws HttpResponseException The indexed one by the provided item.
+   * @see #trapIn(Function)
+   * @see #trapIn(CheckedRunnable)
    */
-  public final <R> R trapIn(final @NonNull Supplier<R> scope) {
+  public final <R> R trapIn(final @NonNull Callable<R> wrap) {
     try {
-      return scope.get();
-    } catch (final RuntimeException e) {
+      return wrap.call();
+    } catch (final Exception e) {
       log.error(e.getMessage());
       throw get();
     }
   }
 
   /**
-   * Traps any potential {@link RuntimeException} thrown out of the provided
-   * scoped code and redirect (re-throw) it as the indexed one.
+   * Traps any potential {@link Exception checked exception} thrown out of the
+   * provided scoped code and redirect (re-throw) it as the indexed one. It is
+   * also a convenient way to write a try-catch block into an one-liner.
    *
-   * @param <T>   Type provided by the functor.
-   * @param <R>   Type of upcoming result from the provided parameter.
-   * @param scope Computation where a {@link RuntimeException} might be thrown
-   *              from.
-   * @return Functor result supplied in the provided parameter.
-   * @see #trapIn(Supplier)
+   * @param wrap Potentially throwing wrapped computation.
+   * @throws HttpResponseException The indexed one by the provided item.
+   * @see #trapIn(Function)
+   * @see #trapIn(Callable)
    */
-  public final <T, R> Function<T, R> trapIn(
-      final @NonNull Function<T, R> scope) {
-    return t -> trapIn(() -> scope.apply(t));
+  public final <E extends Exception> void trapIn(
+      final @NonNull CheckedRunnable<E> wrap) {
+    try {
+      wrap.wrap();
+    } catch (final Exception e) {
+      log.error(e.getMessage(), e);
+      throw get();
+    }
+  }
+
+  /**
+   * Traps any potential {@link Exception checked exception} thrown out of the
+   * provided scoped code and redirect (re-throw) it as the indexed one. It is
+   * also a convenient way to write a try-catch block into an one-liner.
+   *
+   * @param <T> Type provided by the functor.
+   * @param <R> Type of upcoming result from the provided parameter.
+   * @param fun Potentially throwing wrapped computation.
+   * @return Functor result supplied in the provided parameter.
+   * @throws HttpResponseException The indexed one by the provided item.
+   * @see #trapIn(CheckedRunnable)
+   * @see #trapIn(Callable)
+   */
+  public final <T, R> Function<T, R> trapIn(final @NonNull Function<T, R> fun) {
+    return t -> trapIn(() -> fun.apply(t));
   }
 
   /**
    * Provides the indexed exception with its name as the message.
    *
-   * @return An instance of indexed exception.
+   * @return An instance of {@link HttpResponseException indexed exception}.
    */
   @Override
-  public final RuntimeException get() {
+  public final HttpResponseException get() {
     return ex.apply(name());
+  }
+
+  /**
+   * Utility functional interface to suppress a {@link Exception checked
+   * exception}.
+   *
+   * @param <E> Potential exception expected to be thrown.
+   */
+  @FunctionalInterface
+  public interface CheckedRunnable<E extends Exception> {
+    void wrap() throws E;
   }
 }
